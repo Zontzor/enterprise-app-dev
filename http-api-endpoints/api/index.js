371852a3c24d67c612be35f3ebf399481c7db1b4
Zontzip <zontzor@gmail.com>
2017-01-30 21:59:07 +0000

    Add massive db connectivity TODO crud operations

--- http-api-endpoints/api/index.js
+++ http-api-endpoints/api/index.js
@@ -1,5 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var http = require('http');
+var massive = require("massive");
+var connectionString = "postgres://alex:password@localhost/pgguide";
+
+// connect to Massive and get the db instance. You can safely use the
+// convenience sync method here because its on app load
+// you can also use loadSync - it's an alias
+var massiveInstance = massive.connectSync({connectionString : connectionString}) 
+
+// Set a reference to the massive instance on Express' app:
+app.set('db', massiveInstance);
+http.createServer(app).listen(8080);
 
 app.get('/', function (req, res) {
   res.send('Hello World!\n')
