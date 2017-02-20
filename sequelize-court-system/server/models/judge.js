527908ee7e78903e6c6fadb6eaed76cd7e911e17
Zontzip <zontzor@gmail.com>
2017-02-20 23:26:25 +0000

    Update judges table name

--- sequelize-court-system/server/models/judge.js
+++ sequelize-court-system/server/models/judge.js
@@ -3,13 +3,15 @@ module.exports = function(sequelize, DataTypes) {
   const Judge = sequelize.define('Judge', {
     name: DataTypes.STRING,
     room: DataTypes.INTEGER,
-    ext: DataTypes.STRING
+    ext: DataTypes.STRING,
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
-    }
+    },
+    freezeTableName: true,
+    tableName: 'judges'
   });
   return Judge;
 };
