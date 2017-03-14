9a86ec525f4e1cf2f0215bb373e482ecc65f59a2
Zontzip <zontzor@gmail.com>
2017-03-14 20:07:54 +0000

    Add hmac base code

--- hmac-court-system/server/controllers/index.js
+++ hmac-court-system/server/controllers/index.js
@@ -2,14 +2,10 @@ const judges = require('./judges');
 const courtrooms = require('./courtrooms');
 const participants = require('./participants');
 const cases = require('./cases');
-const users = require('./users');
-const authenticate = require('./authenticate');
 
 module.exports = {
   judges,
   courtrooms,
   participants,
-  cases,
-  users,
-  authenticate
+  cases
 };
